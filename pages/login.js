23928c0bbb10e2b3ba3ea99faf2c986106fc41af
Svetlozar Georgiev <svetlozar@Svetlozars-MacBook-Pro.local>
2022-08-10 14:50:52 +0300

    debatts

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,29 @@
 export default function Login() {
   return (
+    // Login Page
     <main className="login-page">
       <h1>Login Page</h1>
+
+      <form action="">
+        {/* Email Address */}
+        <label htmlFor="email">Email Address</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="example@google.com"
+          required
+        />
+        {/* Password */}
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Your password"
+          required
+        />
+        {/* Submit Button */}
+        <button>Login</button>
+      </form>
     </main>
   );
 }
--- pages/register.js
+++ pages/register.js
@@ -1,7 +1,29 @@
 export default function Register() {
   return (
+    // Register Page
     <main className="register-page">
       <h1>Register page</h1>
+
+      <form action="">
+        {/* Email Address */}
+        <label htmlFor="email">Email Address</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="example@google.com"
+          required
+        />
+        {/* Password */}
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Your password"
+          required
+        />
+        {/* Submit Button */}
+        <button>Register</button>
+      </form>
     </main>
   );
 }
