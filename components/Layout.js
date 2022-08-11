06080a45883438f91f26a5e319e6fcb7693d7a67
Tudor Popescu <tudor0404@gmail.com>
2022-08-11 10:51:46 +0100

    small fix, hanged mt to be correct for some pages

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,22 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useRouter } from "next/router";
 
 export default function Layout({ children }) {
+	const router = useRouter();
 	return (
 		<div className="m-0 min-h-screen flex flex-col" id="main-wrapper">
 			<Header />
-			<div className="mt-[150px]">{children}</div>
+			<div
+				className={
+					router.pathname === "/login" ||
+					router.pathname === "/register"
+						? "mt-[110px]"
+						: "mt-[155px]"
+				}
+			>
+				{children}
+			</div>
 			<Footer />
 		</div>
 	);
